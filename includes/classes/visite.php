2e7a03941675f3521ae18bd9078cbb04762cfbe6
mohamedAttefi <attefimohamed0@gmail.com>
2025-12-26 21:27:05 +0100

    modifier la class visite

--- includes/classes/visite.php
+++ includes/classes/visite.php
@@ -26,4 +26,42 @@ class visite
     public function ajouter() {}
     public function modifier($id) {}
     public function supprimer($id) {}
+
+    public function getCount($conn)
+    {
+        $result_stmt = $conn->prepare("SELECT count(*) as total FROM visitesguidees");
+        $result_stmt->execute();
+        return $result_stmt->fetchall()['total'];
+    }
+
+    public function getUpcomingVisites($conn)
+    {
+        $upcoming_visits_stmt = $conn->prepare("
+    SELECT titre, dateheure, COUNT(r.id) as reservations 
+    FROM visitesguidees v 
+    LEFT JOIN reservations r ON v.id = r.idvisite 
+    WHERE v.dateheure >= CURDATE() 
+    GROUP BY v.id 
+    ORDER BY v.dateheure ASC 
+    LIMIT 5
+");
+        $upcoming_visits_stmt->execute();
+
+        return $upcoming_visits_stmt->fetchall();
+    }
+
+    public function getAll($conn, $id)
+    {
+        $result_stmt = $conn->prepare("SELECT * FROM visitesguidees inner join utilisateurs on visitesguidees.id_guide =  utilisateurs.id where utilisateurs.id = $id ORDER BY dateheure DESC");
+        $result_stmt->execute();
+        return $result_stmt->fetchall();
+    }
+
+    public function getVistesParGuide($id, $conn)
+    {
+        $sql = "select * from visitesguidees v join utilisateurs u  on v.id_guide = u.id join reservations r on v.id = r.idvisite where v.id_guide = $id order by v.dateheure limit 1";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
 }
