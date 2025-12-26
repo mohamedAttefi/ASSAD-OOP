35229ffca058358d3a1d4042d30ebf41029b6c4b
mohamedAttefi <attefimohamed0@gmail.com>
2025-12-26 21:27:32 +0100

    modifier la class commentaire

--- includes/classes/commentaire.php
+++ includes/classes/commentaire.php
@@ -7,13 +7,27 @@ class commentaires
     public $note;
     public $text;
     public $date_commentaire;
-    public function __construct($id, $idvisite, $idutilisateur, $note, $text, $date_commentaire)
+    public function __construct($idvisite, $idutilisateur, $note, $text, $date_commentaire)
     {
-        $this->id = $id;
+        $this->id = null;
         $this->idvisite = $idvisite;
         $this->idutilisateur = $idutilisateur;
         $this->note = $note;
         $this->text = $text;
         $this->date_commentaire = $date_commentaire;
     }
+
+
+    public function getCommentaireCount($conn, $id)
+    {
+        $comments_count_stmt = $conn->prepare("
+    SELECT COUNT(*) as total 
+    FROM commentaires 
+    WHERE idutilisateur = $id
+");
+        $comments_count_stmt->execute();
+        return $comments_count_stmt->fetch()['total'];
+    }
+
+    
 }
