25f2be5b3ca739b27cdd14f5790b4e53be437a1b
mohamedAttefi <attefimohamed0@gmail.com>
2025-12-26 21:26:25 +0100

    modifier la class habitat

--- includes/classes/habitat.php
+++ includes/classes/habitat.php
@@ -1,20 +1,116 @@
-<?php 
+<?php
 class habitat
 {
-    public $id;
-    public $nom;
-    public $typeclimat;
-    public $description;
-    public $zonezoo;
-    public function __construct($id, $nom, $typeclimat, $description, $zonezoo)
+    private $id;
+    private $nom;
+    private $typeclimat;
+    private $description;
+    private $zonezoo;
+    public function __construct($nom, $typeclimat, $description, $zonezoo)
     {
-        $this->id = $id;
+        $this->id = null;
         $this->nom = $nom;
         $this->typeclimat = $typeclimat;
         $this->description = $description;
         $this->zonezoo = $zonezoo;
     }
-    public function ajouter() {}
-    public function modifier($id) {}
-    public function supprimer($id) {}
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getTypeclimat()
+    {
+        return $this->typeclimat;
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function getZonezoo()
+    {
+        return $this->zonezoo;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($newId)
+    {
+        $this->id = $newId;
+    }
+
+
+    public function creerHabitat($conn)
+    {
+        $sql = "INSERT INTO habitats 
+        (nom, typeclimat, description, zonezoo)
+        VALUES (?, ?, ?, ?)";
+
+        $stmt = $conn->prepare($sql);
+
+        if (!$stmt) {
+            die("Prepare failed: " . $conn->error);
+        }
+
+        $success = $stmt->execute(
+            [
+                $this->nom,
+                $this->typeclimat,
+                $this->zonezoo,
+                $this->description
+            ]
+        );
+
+        $this->setId($conn->lastInsertId());
+
+        return $success;
+
+        // echo $sql;
+    }
+    public function modifierHabitat($conn, $id)
+    {
+        if (!empty($this->nom)) {
+
+            $modifier = "UPDATE habitats SET nom = ? WHERE id = ?";
+            $stmtMod = $conn->prepare($modifier);
+            $stmtMod->execute([$this->nom, $id]);
+        }
+
+        if (!empty($this->typeclimat)) {
+
+            $modifier = "UPDATE habitats SET typeclimat = ? WHERE id = ?";
+            $stmtMod = $conn->prepare($modifier);
+            $stmtMod->execute([$this->typeclimat, $id]);
+        }
+
+        if (!empty($this->zonezoo)) {
+
+            $modifier = "UPDATE habitats SET zonezoo = ? WHERE id = ?";
+            $stmtMod = $conn->prepare($modifier);
+            $stmtMod->execute([$this->zonezoo, $id]);
+        }
+
+        if (!empty($this->description)) {
+
+            $modifier = "UPDATE habitats SET description = ? WHERE id = ?";
+            $stmtMod = $conn->prepare($modifier);
+            $stmtMod->execute([$this->description, $id]);
+        }
+    }
+    public function supprimerHabitat($id_habitat) {}
+
+
+    public function getAll($conn)
+    {
+        $sql = 'SELECT h.*, COUNT(a.id) as nb_animaux 
+    FROM habitats h 
+    LEFT JOIN animaux a ON h.id = a.id_habitat 
+    GROUP BY h.id 
+    ORDER BY h.nom ASC';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchall();
+    }
 }
