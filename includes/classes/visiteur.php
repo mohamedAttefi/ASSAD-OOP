c2d0de76137080763299c01945c3d3b74d469d54
mohamedAttefi <attefimohamed0@gmail.com>
2025-12-26 21:25:56 +0100

    modifier la class visiteur

--- includes/classes/visiteur.php
+++ includes/classes/visiteur.php
@@ -1,13 +1,56 @@
 <?php
-include "utilisateur.php";
+require_once "utilisateur.php";
 
 class visiteur extends utilisateur
 {
-    public function __construct($nom, $email, $motpasse_hash)
+    private $statut_compte;
+
+    public function __construct($nom, $email, $motpasse_hash, $role, $statut_compte)
     {
-        parent::__construct($nom, $email, $motpasse_hash, "actif", "visiteur");
+        parent::__construct($nom, $email, $motpasse_hash, "visiteur");
+        $this->statut_compte = $statut_compte;
     }
-    public function consulterVisite() {}
-    public function commenter() {}
-    public function chercherVisite() {}
+
+
+    public static function activerUtilisateurs($conn, $id)
+    {
+        return $conn->prepare("update utilisateurs set statut = 'actif' where id = $id and statut = 'desaactive'")->execute();
+    }
+    public static function desactiverUtilisateurs($conn, $id)
+    {
+        return $conn->prepare("update utilisateurs set statut = 'desactive' where id = $id and statut = 'actif'")->execute();
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function getMotpasse()
+    {
+        return $this->motpasse_hash;
+    }
+    public function getRole()
+    {
+        return $this->role;
+    }
+    public function getStatut()
+    {
+        return $this->statut_compte;
+    }
+    public function setStatut($newStatut)
+    {
+        $this->statut_compte = $newStatut;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($newId)
+    {
+        $this->id = $newId;
+    }
 }
